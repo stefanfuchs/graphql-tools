34dc03e512bed7291c30f919bebb2d45ebc67d08
Stefan <stefan1234@gmail.com>
2018-08-24 15:49:06 -0300

    Add dist folder

---
+++ dist/test/tests.d.ts
@@ -0,0 +1,12 @@
+import './testAlternateMergeSchemas';
+import './testDelegateToSchema';
+import './testDirectives';
+import './testErrors';
+import './testFragmentsAreNotDuplicated';
+import './testLogger';
+import './testMakeRemoteExecutableSchema';
+import './testMergeSchemas';
+import './testMocking';
+import './testResolution';
+import './testSchemaGenerator';
+import './testTransforms';
---
+++ dist/test/tests.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require('source-map-support').install();
+require("./testAlternateMergeSchemas");
+require("./testDelegateToSchema");
+require("./testDirectives");
+require("./testErrors");
+require("./testFragmentsAreNotDuplicated");
+require("./testLogger");
+require("./testMakeRemoteExecutableSchema");
+require("./testMergeSchemas");
+require("./testMocking");
+require("./testResolution");
+require("./testSchemaGenerator");
+require("./testTransforms");
+//# sourceMappingURL=tests.js.map
